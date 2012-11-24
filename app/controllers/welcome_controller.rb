1ca004a9c059d4831a9dd8a5d871c05e30e75198
Bèr Kessels <ber@webschuur.com>
2012-11-24 11:06:55 +0100

    Redirect user to login-form when there is only one conference.
    In that case, it makes no sense to let the user choose from conferences.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    # @TODO add tests for this
+    # If only one conference, no need to let people choose, just redirect them.
+    redirect_to new_cfp_session_path if @conferences.count == 1
+  end
 end
