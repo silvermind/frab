f76af41abfe9b8a0857f6d0451cc7242ee41d208
Ruurd Pels <rfpels@gmail.com>
2013-05-23 23:14:49 +0200

    unapply config.force_ssl setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Frab::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Serve everything through SSL
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
