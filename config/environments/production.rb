2ec5ced3c0c4339f8ca90273176c6a893def15bf
Ruurd Pels <rfpels@gmail.com>
2013-05-23 22:24:25 +0200

    roll back changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 Frab::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Serve everything through SSL
-  config.force_ssl = true
-
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
